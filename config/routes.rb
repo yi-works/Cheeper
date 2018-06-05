5fc3801a589ab22ef20a274a1f67be971e741026
Yuki Ito <y.ito.works@gmail.com>
2018-06-05 19:19:45 +0900

    users routing,controllerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'cheeps/index'
-
   root to: 'cheeps#home'
   resources :cheeps do
     collection do
       post :confirm
     end
   end
+
+  resources :users, only: [:new]
 end
