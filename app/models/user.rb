2864b095667c0fa1aca1e1030d07dcda1f0a7448
Yuki Ito <y.ito.works@gmail.com>
2018-06-06 17:37:47 +0900

    バリデーション パスワード長の空白時エラーを調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
 
   before_save { email.downcase! }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6, allow_blank: true }
 end
