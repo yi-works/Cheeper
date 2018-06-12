e5ed9db1660b3a03a67ef9693c7cd80516f35c75
Yuki Ito <y.ito.works@gmail.com>
2018-06-12 19:51:12 +0900

    デザインを調整

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,7 @@
 class Contact < ApplicationRecord
 
-  validates :content
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :content, presence: true, length: { maximum: 1000 }
 end
