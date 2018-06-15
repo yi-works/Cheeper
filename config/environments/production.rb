3df0da3d781bd00d2ca2c6b846b19bf3d6dea7ef
Yuki Ito <y.ito.works@gmail.com>
2018-06-15 19:17:40 +0900

    letter_opener本番環境用設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # config.action_mailer.default_url_options = { host: 'https://shielded-mountain-61778.herokuapp.com/' }
   config.action_mailer.default_url_options = { host: 'shielded-mountain-61778.herokuapp.com' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
